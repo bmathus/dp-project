998c6a3ac2d4110c2d46fe75f77dcde1c08ce309
Matus Bojko <bojko.matus@gmail.com>
2024-08-28 22:58:24 +0200

    saving checkpoints during training

--- project/trainer.py
+++ project/trainer.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from tqdm import tqdm
 from project.logging import Statistics
 from project.logging import Log
+from project.datamodule import DataModule
+from pathlib import Path
 
 
 def decide_device():
@@ -20,7 +22,8 @@ class Trainer:
 
         # Setup model and config
         self.cfg = cfg
-        self.model = model.to(self.device)  # Move parameters to GPU
+        self.start_epoch = 0
+        self.model: nn.Module = model.to(self.device)  # Move parameters to GPU
 
         # Create optimizer
         self.opt = torch.optim.SGD(
@@ -29,45 +32,51 @@ class Trainer:
         )
 
         # Create loss function
-        self.loss = nn.CrossEntropyLoss()
+        self.loss_fun = nn.CrossEntropyLoss()
 
-    def setup(self, datamodule, log: Log):
+    def setup(self, datamodule: DataModule, log: Log):
         self.log = log
         self.datamodule = datamodule
         self.datamodule.setup(self.cfg)
 
-    def fit(self):
+
+    def fit(self, experiment_path: Path):
         # Implement training loop
         self.log.on_training_start()
-        for epoch in range(self.cfg.max_epochs):
+        for epoch in range(self.start_epoch,self.cfg.max_epochs):
 
             # Training phase
-            stats_train = Statistics()
-            self.train_epoch(
+            train_stats = Statistics()
+            self._train_epoch(
                 epoch, 
                 model=self.model, 
                 dataloader=self.datamodule.dataloader_train, 
-                stats=stats_train
+                train_stats=train_stats
             )
 
             # Validation phase
-            stats_val = Statistics()
-            self.validate_epoch(
+            valid_stats = Statistics()
+            self._validate_epoch(
                 epoch, 
                 model=self.model, 
                 dataloader=self.datamodule.dataloader_val, 
-                stats=stats_val
+                valid_stats=valid_stats
             )
 
-            #Log epoch results
+            #Log epoch metrics
             self.log.on_epoch_complete(
                 epoch=epoch,
-                stats=Statistics.merge(stats_train,stats_val)
+                stats=Statistics.merge(train_stats,valid_stats)
             )
+
+            # Save latest models and create checkpoints
+            self._save_checkpoint(epoch,experiment_path,file_name="latest.pt")
+            if epoch % self.cfg.checkpoint_freq == 0:
+                self._save_checkpoint(epoch,experiment_path, file_name=f"checkpoint-{epoch:04d}.pt")
 
         self.log.on_training_stop()
 
-    def train_epoch(self, epoch, model, dataloader, stats):
+    def _train_epoch(self, epoch: int, model, dataloader, train_stats: Statistics):
         with tqdm(dataloader, desc=f"Train: {epoch}") as progress:
             for x,y in progress:
 
@@ -77,19 +86,19 @@ class Trainer:
 
                 # Forward pass
                 y_hat_logits = model(x)
-                l = self.loss(y_hat_logits, y)
+                loss = self.loss_fun(y_hat_logits, y)
 
                 # Backward pass & Update params
                 self.opt.zero_grad()
-                l.backward()
+                loss.backward()
                 self.opt.step()
 
                 # Update statistics
-                stats.step("loss_train", l.item())
-                progress.set_postfix(stats.get())
+                train_stats.train_step(loss)
+                progress.set_postfix(train_stats.get_averages())
        
        
-    def validate_epoch(self, epoch, model, dataloader, stats):        
+    def _validate_epoch(self, epoch: int, model, dataloader, valid_stats: Statistics):        
         with torch.no_grad():       # We don't need gradients in validation
             with tqdm(dataloader, desc=f"Val: {epoch}") as progress:
                 for x,y in progress:
@@ -99,8 +108,22 @@ class Trainer:
 
                     # Forward pass
                     y_hat_logits = model(x)
-                    l = self.loss(y_hat_logits, y)
+                    loss = self.loss_fun(y_hat_logits, y)
 
                     # Update statistics
-                    stats.step("loss_val", l.item())
-                    progress.set_postfix(stats.get())
+                    valid_stats.valid_step(loss)
+                    progress.set_postfix(valid_stats.get_averages())
+    
+    def _save_checkpoint(self,epoch: int, experiment_path: Path, file_name: str):
+        checkpoint = {
+            "model": self.model.state_dict(),
+            "optimizer": self.opt.state_dict(),
+            "epoch": epoch
+        }
+
+        #Create checkpoint foelder if does not exist
+        checkpoint_path = experiment_path / "checkpoints" / file_name
+        checkpoint_path.parent.mkdir(parents=True, exist_ok=True)
+
+        print(f"Saving checkpoint: {checkpoint_path.as_posix()}")
+        torch.save(checkpoint, checkpoint_path.as_posix())
