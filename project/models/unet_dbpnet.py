7235d5ce6149503ec13afccfcfbe81e71b176dff
Matus Bojko <bojko.matus@gmail.com>
2025-05-04 17:16:36 +0200

    d1-nearest-d2-bilinear

--- project/models/unet_dbpnet.py
+++ project/models/unet_dbpnet.py
@@ -62,13 +62,13 @@ class DBPNet(nn.Module):
                   'feature_chns': [16, 32, 64, 128, 256],
                   'dropout': [0.05, 0.1, 0.2, 0.3, 0.5],
                   'class_num': class_num,
-                  'up_type': 1,
+                  'up_type': 2,
                   'acti_func': 'relu'}
         params2 = {'in_chns': in_chns,
                   'feature_chns': [16, 32, 64, 128, 256],
                   'dropout': [0.05, 0.1, 0.2, 0.3, 0.5],
                   'class_num': class_num,
-                  'up_type': 2,
+                  'up_type': 1,
                   'acti_func': 'relu'}
         
         self.encoder = Encoder(params1)
@@ -94,7 +94,7 @@ class UNet_DBP(nn.Module):
                 'feature_chns': [16, 32, 64, 128, 256],
                 'dropout': [0.05, 0.1, 0.2, 0.3, 0.5],
                 'class_num': class_num,
-                'up_type': 1,
+                'up_type': 2,
                 'acti_func': 'relu'}
         
 
